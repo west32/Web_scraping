eb807a5da695e33b90f0493dc63986c21aa1418c
Bartłomiej Juda <bartekjkk@gmail.com>
2021-11-10 14:50:35 +0700

    web scraps

---
+++ New_york_times_articles_b4.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+
+
+#
+# Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on
+# the New York Times homepage.
+
+def article_title_h3(tag):
+    return tag.name == "h3"
+
+
+class TheNewYorkTimesParser():
+    PAGE_URL = "https://www.nytimes.com/"
+
+    def __init__(self):
+        self.parsed_page = None
+        self.last_post_index = 0
+        self.found_post_preview = []
+
+    def load_page(self):
+        response = requests.get(self.PAGE_URL)
+        response.raise_for_status()
+        self.parsed_page = BeautifulSoup(response.text, features="html.parser")
+
+    def parse_all_preview(self):
+        article_titles = self.parsed_page.find_all("h3")
+        for article_title in article_titles:
+            self.found_post_preview.append(article_title.string)
+
+
+def run_example():
+    parser = TheNewYorkTimesParser()
+    parser.load_page()
+    parser.parse_all_preview()
+    for post in parser.found_post_preview:
+        print(post)
+
+
+if __name__ == "__main__":
+    run_example()
---
+++ infosharecourses_parser.py
@@ -0,0 +1,60 @@
+import requests
+from dataclasses import dataclass
+from bs4 import BeautifulSoup
+
+@dataclass
+class CoursePreview:
+    title: str
+    time:str
+    type: str
+
+def course_title_div(tag):
+     return tag.name == "div" and tag.has_attr("class") and "courses-page__item__inner" in tag["class"]
+
+
+
+class InfoShareParser:
+    PAGE_URL = "https://infoshareacademy.com"
+
+    def __init__(self):
+        self.parsed_page = None
+        self.last_post_index = 0
+        self.found_course_previews= []
+
+    def load_page(self):
+        response = requests.get(self.PAGE_URL)
+        response.raise_for_status()
+        self.parsed_page= BeautifulSoup(response.text, features="html.parser")
+
+    def parse_all_previews(self):
+        course_titles = self.parsed_page.find_all(course_title_div)
+        for course_title in course_titles:
+            title_tag = course_title.p
+            title= title_tag.string
+            span_tag = course_title.find_all("span")
+            time= span_tag[0].string
+            type = span_tag[1].string
+            course_preview= CoursePreview(title,time,type)
+            self.found_course_previews.append(course_preview)
+
+
+
+
+
+
+
+    # def check(self):
+    #     title_tag = self.parsed_page.find_all(is_title_contain_tag)
+    #
+    #     print(title_tag.string)
+
+
+def run_example():
+    parser = InfoShareParser()
+    parser.load_page()
+    parser.parse_all_previews()
+    for article in parser.found_course_previews:
+        print(f"course title = '{article.title}', type = {article.type}, duration = {article.time}")
+
+if __name__=="__main__":
+    run_example()
